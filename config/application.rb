377449ffa1a824821753a763ba49d9730aacfed4
KMZ0209 <sedmikrasky0209@gmail.com>
2023-05-15 01:20:51 +0900

    I18nAppの設定を削除しました

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BooksApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -20,9 +20,8 @@ module BooksApp
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-module I18nApp
-  class Application < Rails::Application
-    config.i18n.default_locale = :ja # 追加
-  end
-end
-config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+# module I18nApp
+#   class Application < Rails::Application
+#     # config.i18n.default_locale = :ja # 追加
+#   end
+# end
