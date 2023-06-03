47c15f1341a3b5d8377a14b56ac1fcf8c1a3bc45
KMZ0209 <sedmikrasky0209@gmail.com>
2023-06-03 23:11:38 +0900

    ご指摘頂いた箇所を修正しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
-  # ログインしていない時に登録した内容を確認できないようにする
   before_action :authenticate_user!
   # コントローラーで許可されたパラメーターを設定するためのフィルター
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -15,24 +13,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code address self_introduction])
   end
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
-
   private
-
-  def after_sign_in_path_for(*)
-    books_path
-  end
-
-  def after_sign_out_path_for(*)
-    new_user_session_path
-  end
 
   def signed_in_root_path(*)
     user_path(current_user)
