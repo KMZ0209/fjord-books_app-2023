4cb3288895e8dfbd09ef535efa056d07e232f135
KMZ0209 <sedmikrasky0209@gmail.com>
2023-09-01 21:40:02 +0900

    不要だった機能追加を削除、自動生成されたものを元に戻しました

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -3,5 +3,6 @@
 class ReportMention < ApplicationRecord
   belongs_to :mention_to, class_name: 'Report'
   belongs_to :mentioned_by, class_name: 'Report'
+
   validates :mentioned_by_id, uniqueness: { scope: :mention_to_id }
 end
