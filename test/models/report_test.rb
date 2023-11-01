89470506730bb38f5314fc5a804d3ffa59166023
KMZ0209 <sedmikrasky0209@gmail.com>
2023-11-02 02:02:04 +0900

    report_test.rb内のアサーションの内容を変更しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,14 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test '#editable?' do
-    report = reports(:alices_report)
-    target_user = users(:alice)
-    assert report.editable?(target_user)
+  # test '#editable?' do
+  #   report = reports(:alices_report)
+  #   target_user = users(:alice)
+  #   assert report.editable?(target_user)
 
-    target_user = users(:bob)
-    assert_not report.editable?(target_user)
-  end
+  #   target_user = users(:bob)
+  #   assert_not report.editable?(target_user)
+  # end
 
   test '#created_on' do
     report = reports(:alices_report)
@@ -18,15 +18,15 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test '日報保存後に言及機能が正しく設定されているか確認' do
-    user = users(:carol)
-    mentioned_report = reports(:bobs_report)
-    mentioning_report = Report.new(title: '言及テスト', content: 'http://localhost:3000/reports/2', user:)
+    bobs_report = reports(:bobs_report)
+    carols_report = reports(:carols_report)
+    daisys_report = Report.new(title: '言及テスト', content: "http://localhost:3000/reports/#{bobs_report.id}", user: users(:daisy))
 
     assert_difference 'ReportMention.count', 1 do
-      mentioning_report.save
+      daisys_report.save
     end
 
-    assert_equal [reports(:carols_report)], mentioned_report.mentioning_reports
-    assert_equal [], mentioning_report.mentioned_reports
+    assert_equal [carols_report, daisys_report], bobs_report.mentioned_reports
+    assert_equal [bobs_report], daisys_report.mentioning_reports
   end
 end
